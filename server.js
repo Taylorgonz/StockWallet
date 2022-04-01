e025702615a37bdcc11fff168af8fc3ceedc9966
taylorgonz <hello@taylorgonz.com>
2022-04-01 18:27:33 -0400

    server connected

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const sequelize = require('./config/connection');
+// const models = require('/models');
+// const routes = require('./routes');
+
+app.use([
+    express.urlencoded({extended: true}),
+    express.json()
+])
+// if (process.env.NODE.ENV === "production") {
+//     app.use(express.static("client/build"));
+
+// }
+// app.use(routes);
+
+sequelize
+    .sync({force: false, logging: true })
+    .then(()=> {
+        app.listen(PORT, () => {
+            console.log("Server now on port", PORT);
+
+        });
+    })
+    .catch(err=> console.error(err));
